fb1b867af9ff2fdd9b5406c19153569fbafd0fd7
IDRIS ABDELLAH <idrisabdellah@outlook.fr>
2023-01-23 00:45:15 +0100

    refactor

--- etl.py
+++ etl.py
@@ -2,12 +2,11 @@ from extract import extract_impl
 from transform import transform_impl
 
 
-news = []
+news = {}
 
-tweets = extract_impl.extract_data('EmmanuelMacron', 1)
-tweet = transform_impl.tweet_transfomation('EmmanuelMacron', tweets)
+if __name__ == '__main__' :
+    tweets = extract_impl.extract_data('EmmanuelMacron', 1)
+    tweet = transform_impl.tweet_transfomation('EmmanuelMacron', tweets)
 
-news.append(tweet)
-
-for new in news:
-    print(new)
+    for new in news:
+        print(new)
