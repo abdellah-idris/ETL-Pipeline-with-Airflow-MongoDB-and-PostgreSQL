0841f4015b93815c57b30ad10997b0a7967c3306
IDRIS ABDELLAH <50142372+abdellah-idris@users.noreply.github.com>
2023-01-22 15:26:30 +0000

    add extrcat file

---
+++ extract/extract.py
@@ -0,0 +1,15 @@
+import os
+import tweepy
+
+from dotenv import load_dotenv
+load_dotenv()
+
+consumer_key = os.environ.get('CONSUMER_KEY')
+consumer_secret = os.environ.get('CONSUMER_SECRET')
+access_token = os.environ.get('ACCESS_TOKEN')
+access_token_secret = os.environ.get('ACCESS_TOKEN_SECRET')
+
+auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+auth.set_access_token(access_token, access_token_secret)
+api = tweepy.API(auth)
+
